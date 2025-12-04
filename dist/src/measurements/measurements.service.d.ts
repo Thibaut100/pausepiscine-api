3f85fc2259495f6bf85f04833ea2d8725b26199e
Thibaut <thibaut@Mac.lan>
2025-12-04 16:11:29 +0100

    Exclude measurements module from build

--- dist/src/measurements/measurements.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-import { Measurement } from '@prisma/client';
-export declare class MeasurementsService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    create(userId: string, data: any): Promise<Measurement>;
-    findAll(userId: string, poolId?: string): Promise<Measurement[]>;
-    findOne(id: string, userId: string): Promise<Measurement | null>;
-}
